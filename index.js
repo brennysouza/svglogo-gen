1496c2048136760731b17ee29e1584bc9fd406b4
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-08-26 14:50:51 -0400

    fixed logo where monogram now appears above the shape

--- index.js
+++ index.js
@@ -1,77 +1,117 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const logo = require('./logo.svg');
+// const logo = require('./logo.svg');
 // This code below imports the parent and child classes of the shapes allowing them to be used here in this file. 
-const { Triangle, Circle, Square } = require('./lib/shapes');
-
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
 
 const prompt = [
     {
         type: 'input',
         message: 'Please enter a monogram of three letters that best represents your brand, logo, or business.',
-        name: 'Monogram',
+        name: 'monogram',
     },
     {
         type: 'input',
         message: 'Please enter in a color for your logo monogram.',
-        name: 'Monogram Color',
+        name: 'monogramColor',
     },
     {
         type: 'list',
         message: 'Please choose a logo shape to use for your logo.',
-        name: 'Shape',
+        name: 'shape',
         choices: ['Circle', 'Square', 'Triangle'],
     },
     {
         type: 'input',
         message: 'Please enter in a color for your logo shape.',
-        name: 'Shape Color',
+        name: 'shapeColor',
     },
 ];
 
-function generateSvgLogo(shape, shapeColor, monogram, monogramColor) {
-    const logo = new shape();
-
+function generateSvgLogo(shapeClass, shapeColor, monogram, monogramColor) {
+    const logo = new shapeClass();
     logo.setColor(shapeColor);
-
     const shapeSvg = logo.render();
 
-    const monogramSvg = `<text x="20" y="40" font-size="30" fill="${monogramColor}">${monogram}</text>`;
+    // Thise code is for the position and font size of the text based on the shape
+    let textX, textY, fontSize;
+
+        switch (shapeClass.name) {
+            case 'Circle':
+                textX = 100; 
+                textY = 100; 
+                fontSize = 50;
+                break;
+            case 'Square':
+                textX = 150; 
+                textY = 150; 
+                fontSize = 50;
+                break;
+            case 'Triangle':
+                textX = 150; 
+                textY = 110; 
+                fontSize = 50;
+                break;
+        }
+
+    // console.log(`Debug - textX: ${textX}, textY: ${textY}, fontSize: ${fontSize}`);
+
+    const monogramSvg = `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="40" fill="${monogramColor}">${monogram}</text>`;
+
 
     return `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      ${monogramSvg}
       ${shapeSvg}
+      ${monogramSvg}
     </svg>
   `;
 }
 
-function init() {
-    inquirer.prompt(prompt).then((responses) => { 
-        const { Shape, 'Shape Color': shapeColor, Monogram, 'Monogram Color': monogramColor } = responses; {
-            let shape;
-            switch (Shape) {
+async function init() {
+    // inquirer.prompt(prompt).then((responses) => { 
+    //     const { Shape, 'Shape Color': shapeColor, Monogram, 'Monogram Color': monogramColor } = responses; 
+    //         let shape;
+    //         switch (Shape) {
+    //             case 'Circle':
+    //                 shape = Circle;
+    //                 break;
+    //             case 'Square':
+    //                 shape = Square;
+    //                 break;
+    //             case 'Triangle':
+    //                 shape = Triangle;
+    //                 break;
+
+    try {
+        const responses = await inquirer.prompt(prompt);
+
+        const { shape, shapeColor, Monogram, monogramColor } = responses; 
+        
+        let shapeClass;
+            switch (shape) {
                 case 'Circle':
-                    shape = Circle;
+                    shapeClass = Circle;
                     break;
                 case 'Square':
-                    shape = Square;
+                    shapeClass = Square;
                     break;
                 case 'Triangle':
-                    shape = Triangle;
+                    shapeClass = Triangle;
                     break;
             }
 
-        const logoSvgContent = generateSvgLogo(shape, shapeColor, Monogram, monogramColor);
+        const logoSvgContent = generateSvgLogo(shapeClass, shapeColor, Monogram, monogramColor);
         fs.writeFileSync('logo.svg', logoSvgContent);
         console.log('Generated logo.svg');
 
-        }
+        } catch (error) {
+            console.error('An error occurred:', error);
 
+        }
+        
         // writeToFile('logo.svg', logoContent);
-    });
-}
+    }
 
 // Function call to initialize app
 init();
